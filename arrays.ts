71ee8674943a423a8795b4a7fa2b03ef7b9e386a
Anandhu M S <anandhumeleveettil@gmail.com>
2022-12-07 14:43:55 +0530

    arrays

---
+++ arrays.ts
@@ -0,0 +1,11 @@
+const names: string[] = [];
+names.push("Anandhu");
+
+// Readonly arrays
+let readonlyname: readonly string[] = ["Demo"];
+// readonlyname.push("Anandhu")
+
+
+// inference
+
+let inferenceArray = 20;
--- special_types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var firstname;
-firstname = "Anandhu";
-// Any
-var firstnameAny;
-firstnameAny = "Anandhu";
