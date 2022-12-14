3147970856ad2e551b6250ebb6f00094ed4da814
Anandhu M S <anandhumeleveettil@gmail.com>
2022-12-14 09:56:53 +0530

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  public name = "anandhu";
+  public data = {
+    name: "anandhu",
+    age: 21,
+    domain: "MEAN STACK"
+  }
 }
